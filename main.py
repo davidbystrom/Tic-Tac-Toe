7e5ff0aa7873ffed0f43f8562e312882be89b379
David <github@dbystrom.com>
2024-03-21 18:57:37 +0100

    MinMax Algo With Alpha Beta pruning

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import copy
 class State():
     def __init__(self, state=[['#', '#', '#'],['#', '#', '#'],['#', '#', '#']]):
         self.state = state
@@ -24,12 +25,12 @@ def player(state: State):
     return 'X' if x == o else 'O'
 
 def result (state: State, move: tuple[int, int, str]):
-    board = state.state
+    board = copy.deepcopy(state.state)
     board[move[0]][move[1]] = move[2]
     return State(board)
 
 def terminal(state: State):
-    board = state.state
+    board = copy.deepcopy(state.state)
     for i in range(3):
         if (len(list(filter((lambda x: x == 'X'), board[i]))) == 3) or (len(list(filter((lambda x : x == 'O'), board[i]))) == 3):
             return True
@@ -37,7 +38,82 @@ def terminal(state: State):
             return True
     if (board[0][0] == board[1][1] == board[2][2] == 'X' or board[0][0] == board[1][1] == board[2][2] == 'O') or (board[2][0] == board[1][1] == board[0][2] == 'X' or board[2][0] == board[1][1] == board[0][2] == 'O'):
         return True
+    if len(actions(state)) == 0:
+        return True
     return False
 
+def score(state: State):
+    board = copy.deepcopy(state.state)
+    for i in range(3):
+        if (len(list(filter((lambda x: x == 'X'), board[i]))) == 3):
+            return 1
+        elif (len(list(filter((lambda x: x == 'O'), board[i]))) == 3):
+            return -1
+        elif board[0][i] == board[1][i] == board[2][i] == 'X':
+            return 1
+        elif board[0][i] == board[1][i] == board[2][i] == 'O':
+            return -1
+    if (board[0][0] == board[1][1] == board[2][2] == 'X') or (board[2][0] == board[1][1] == board[0][2] == 'X'):
+        return 1
+    elif (board[0][0] == board[1][1] == board[2][2] == 'O') or (board[2][0] == board[1][1] == board[0][2] == 'O'):
+        return -1
+    else:
+        return 0
+
+def maxValue(state: State, alpha=-float('inf'), beta=float('inf')):
+    if terminal(state):
+        return score(state)
+    v = -float('inf')
+    #l = len(actions(state))
+    for a in actions(state):
+        v = max(v, minValue(result(state, a + ('X',)), alpha, beta))
+        if v >= beta:
+            break
+        alpha= max(alpha, v)
+    return v
+
+def minValue(state: State, alpha=-float('inf'), beta=float('inf')):
+    if terminal(state):
+        return score(state)
+    v = float('inf')
+    #l = len(actions(state))
+    for a in actions(state):
+        v = min(v,maxValue(result(state, a + ('O',)), alpha, beta))
+        if v <= alpha:
+            break
+        beta = max(beta, v)
+    return v
+
+def AI(state: State):
+    a = actions(state)
+    mini = float('inf')
+    best = None
+    for action in a:
+        maxv = maxValue(result(state, action + ('O',)))
+        if maxv < mini:
+            mini = maxv
+            best = action
+    return result(state, best + ('O',))
+
 if __name__ == '__main__':
-    s0 = State()
+    s0 = State([['#', '#', '#'], ['#', '#', '#'], ['#', '#', '#']])
+    state = s0
+    print(state)
+    while True:
+        print("Player Turn")
+        row = int(input("Row: "))
+        col = int(input("Col: "))
+        state = result(state, (row, col, 'X'))
+        if terminal(state):
+            print("Player Winns") if score(state) == 1 else print("Tie!")
+            break
+        print(state)
+        print("AI Turn:")
+        state = AI(state)
+        print(state)
+        if terminal(state):
+            print("AI Winns") if score(state) == -1 else print("Tie!")
+            break
+
+
+    #print(result(s0, best + ('O',)))
